e25ff5b09add2ab0635cfcf50d6aee794101d8f8
Nikolay Kozhuharski <nikolay.kozhuharski@americaneagle.com>
2023-08-03 16:32:18 +0300

    start the project, add fake db and ajax requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { uuid } from "./helpers/uuid";
 
-function App() {
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  function handleAdd() {
+    postData();
+  }
+  function handleGet() {
+    getData();
+  }
+
+  const postData = (object = "Car", expense = 35) => {
+    axios
+      .post(`http://localhost:3030/Nikolay`, {
+        id: uuid(),
+        subject: object,
+        expenses: expense,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const getData = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3030/Nikolay", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setExpenses(result))
+      .catch((error) => console.log("error", error));
+  };
+  // const values = expenses.map((expense) => {
+  //   return {}
+  // })
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {expenses?.map((expense) => (
+        <div>
+          <h3>
+            <span>{expense.subject}</span>
+            <button onClick={handleAdd}>ADD</button>
+            <button onClick={handleGet}>GET</button>
+          </h3>
+          <p>{expense.expenses}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
